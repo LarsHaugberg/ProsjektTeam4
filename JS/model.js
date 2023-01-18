9acb2b5b439041ae63ef26ac90d61b22b7fe530a
SimenKSM <110755778+SimenKSM@users.noreply.github.com>
2023-01-18 13:36:32 +0100

    ye

--- JS/model.js
+++ JS/model.js
@@ -1,10 +1,63 @@
 const model = {
 
     app: {
-        currentPage: 'fronstide'
+        currentPage: 'frontside',
+        currentUser: '',
     },
-    inputs: {
 
+    inputs: {
+        frontPage: {
+
+            currentPicture: '',
+
+        },
+
+        bookingPage: {
+
+            fleetChoice: '',
+            packageChoice: '',
+            comfortChoice: [],
+            selectedDate: [],
+            selectedHour: [],
+
+        },
+        blogPage: {
+
+            currentPost: '',
+
+        },
+
+    },
+
+    data: {
+        packageOptions: [
+            {
+                name: '',
+                price: 0,
+            },
+        ],
+        comfort: [
+            {
+                Id: 1,
+                name: 'produkt1',
+                price: 0,
+            },
+        ],
+        
+        bookings: [
+            {
+                orderId: 1,
+                fleetId: [],
+                chosenComforts: [],
+                chosenDates:[],
+                chosenHours:[],
+                customer: '',
+        },
+    ],
+        shownPicture: [],
+        blogPosts: [],
+        packages: ['pakke1'],
+        fleets: ['båt1'],
     },
 
 
